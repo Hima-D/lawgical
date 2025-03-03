525b813bb8468662506d67e18f6555f0ce51f140
Hima-D <dixithimanshu012@gmail.com>
2025-03-04 01:57:28 +0530

    Remove duplicate import of SpeedInsights in layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 
 // Google Fonts
