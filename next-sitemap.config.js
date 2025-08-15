6247d5656218f99cf0b289eb365a50020fa19b26
Himanshu Dixit <himanshu.dixit@carer.ai>
2025-08-15 17:07:18 +0530

    added sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.lawgical.tech',
+  generateRobotsTxt: true, // optional
+  sitemapSize: 5000,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/secret-page'], // pages you don’t want in sitemap
+  robotsTxtOptions: {
+    policies: [{ userAgent: '*', allow: '/' }],
+  },
+};
